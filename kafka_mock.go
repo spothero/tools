fe0538bb451decbc26f3359b6797ce68a35e4a4c
Brian Charous <brian.charous@spothero.com>
2019-02-25 11:14:07 -0600

    fix mock

--- kafka_mock.go
+++ kafka_mock.go
@@ -18,7 +18,7 @@ func (m MockKafkaConsumer) ConsumeTopic(ctx context.Context, handler KafkaMessag
 
 // ConsumeTopicFromBeginning mocks the Kafka consumer ConsumeTopicFromBeginning method
 func (m MockKafkaConsumer) ConsumeTopicFromBeginning(ctx context.Context, handler KafkaMessageHandler, topic string, readResult chan PartitionOffsets, catchupWg *sync.WaitGroup, exitAfterCaughtUp bool) error {
-	return m.Called(handler, topic, topic, readResult, catchupWg, exitAfterCaughtUp).Error(0)
+	return m.Called(handler, topic, readResult, catchupWg, exitAfterCaughtUp).Error(0)
 }
 
 // ConsumeTopicFromLatest mocks the Kafka consumer ConsumeTopicFromLatest method
