f1a50ee338173aa723f66aeed99cbe60b3474eda
Brian Charous <brian.charous@spothero.com>
2019-03-14 13:56:42 -0500

    mutex mock kafka consumer read result channel

--- kafka_mock.go
+++ kafka_mock.go
@@ -10,26 +10,33 @@ import (
 // MockKafkaConsumer implements KafkaConsumerIface for testing purposes
 type MockKafkaConsumer struct {
 	mock.Mock
+	sync.Mutex
 	readResult chan PartitionOffsets
 }
 
 // ConsumeTopic mocks the Kafka consumer ConsumeTopic method
 func (m *MockKafkaConsumer) ConsumeTopic(ctx context.Context, handler KafkaMessageHandler, topic string, offsets PartitionOffsets, readResult chan PartitionOffsets, catchupWg *sync.WaitGroup, exitAfterCaughtUp bool) error {
 	catchupWg.Done()
+	m.Lock()
 	m.readResult = readResult
+	m.Unlock()
 	return m.Called(handler, topic, offsets, readResult, catchupWg, exitAfterCaughtUp).Error(0)
 }
 
 // ConsumeTopicFromBeginning mocks the Kafka consumer ConsumeTopicFromBeginning method
 func (m *MockKafkaConsumer) ConsumeTopicFromBeginning(ctx context.Context, handler KafkaMessageHandler, topic string, readResult chan PartitionOffsets, catchupWg *sync.WaitGroup, exitAfterCaughtUp bool) error {
 	catchupWg.Done()
+	m.Lock()
 	m.readResult = readResult
+	m.Unlock()
 	return m.Called(handler, topic, readResult, catchupWg, exitAfterCaughtUp).Error(0)
 }
 
 // ConsumeTopicFromLatest mocks the Kafka consumer ConsumeTopicFromLatest method
 func (m *MockKafkaConsumer) ConsumeTopicFromLatest(ctx context.Context, handler KafkaMessageHandler, topic string, readResult chan PartitionOffsets) error {
+	m.Lock()
 	m.readResult = readResult
+	m.Unlock()
 	return m.Called(handler, topic, readResult).Error(0)
 }
 
