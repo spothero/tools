0028ef13c1dd707c3b3d820d279b3c09793ae687
Benjamin Goldberg <ben@spothero.com>
2019-09-12 08:29:00 -0500

    update kafka consumer flags to be consistent

--- kafka/consumer.go
+++ kafka/consumer.go
@@ -69,7 +69,7 @@ func NewDefaultConsumerConfig() ConsumerConfig {
 
 // Registers high-level consumer flags with pflags
 func (c *ConsumerConfig) RegisterFlags(flags *pflag.FlagSet) {
-	flags.BoolVar(&c.JSONEnabled, "enable-json", true, "When this flag is set, messages from Kafka will be consumed as JSON instead of Avro")
+	flags.BoolVar(&c.JSONEnabled, "kafka-consumer-enable-json", true, "When this flag is set, messages from Kafka will be consumed as JSON instead of Avro")
 	c.SchemaRegistry.RegisterFlags(flags)
 }
 
