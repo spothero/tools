8d768e3c33ea787ea5690fa11295afcefd88208a
Benjamin Goldberg <benjigoldberg@users.noreply.github.com>
2019-12-13 11:29:46 -0600

    Examples >example test.go (#89)
    Merged by your friendly neighborhood SpotBot ✅

---
+++ jose/example_test.go
@@ -0,0 +1,60 @@
+// Copyright 2019 SpotHero
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package jose_test
+
+import (
+	"fmt"
+
+	"github.com/spothero/tools/jose"
+)
+
+func Example() {
+	// The JOSE Config allows users to configure the JOSE client for use with their OIDC provider
+	c := jose.Config{
+		// The "JWKS" endpoint for retrieving key sets for JWT verification
+		JSONWebKeySetURL: "https://your-oidc-provider/.well-known/jwks.json",
+		// ValidIssuer is the URL of the JWT issuer for this environment. This must match the `iss`
+		// within the JWT claim
+		ValidIssuer: "https://your-oidc-provider/issuerID",
+		// ClaimGenerators determine how JWT claims are to be parsed.
+		ClaimGenerators: []jose.ClaimGenerator{
+			jose.CognitoGenerator{},
+		},
+		// If true, any HTTP or GRPC middleware will return unauthenticated errors for missing or
+		// invalid JWTs
+		AuthRequired: true,
+	}
+
+	// Instantiate the JOSE provider
+	client, err := c.NewJOSE()
+	if err != nil {
+		fmt.Println(fmt.Errorf("failed to create the JOSE client: %w", err))
+	}
+
+	// With the instantiated client, callers may choose to add HTTP Middleware and GRPC
+	// interceptors directly to their servers:
+	//
+	// httpMiddleware := jose.GetHTTPMiddleware(client, c.AuthRequired),
+	//
+	// joseInterceptorFunc := jose.GetContextAuth(client, c.AuthRequired)
+	// grpcauth.UnaryServerInterceptor(joseInterceptorFunc)
+
+	// The instantiated client may also be used directly to parse and validate JWTs
+	claims := client.GetClaims()
+	if err := client.ParseValidateJWT("<your-jwt>", claims...); err != nil {
+		fmt.Println(fmt.Errorf("failed to parse token: %w", err))
+	}
+	fmt.Printf("successfully parsed token: %+v\n", claims[0])
+}
