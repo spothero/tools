f3c2aa841426096dbe0c2f77197ccc7c5b3931ed
Benjamin Goldberg <ben@spothero.com>
2020-01-06 15:33:40 -0600

    auth and default client refactor

--- http/client.go
+++ http/client.go
@@ -20,30 +20,11 @@ import (
 	"time"
 
 	"github.com/cenkalti/backoff/v4"
+	"github.com/spothero/tools/jose"
 	"github.com/spothero/tools/log"
+	"github.com/spothero/tools/tracing"
 	"go.uber.org/zap"
 )
-
-// ClientMiddleware defines an HTTP Client middleware function. The function is called prior to
-// invoking the transport roundtrip, and the returned response function is called after the
-// response has been received from the client.
-type ClientMiddleware func(*http.Request) (*http.Request, func(*http.Response) error, error)
-
-// MiddlewareRoundTripper implements a proxied net/http RoundTripper so that http requests may be decorated
-// with middleware
-type MiddlewareRoundTripper struct {
-	RoundTripper http.RoundTripper
-	// Middleware consist of a function which is called prior to the execution of a request. This
-	// function returns the potentially modified request, the post-response handler, and an error,
-	// if any. The response handler is invoked after the HTTP request has been made.
-	//
-	// Middleware are called in the order they are specified. In otherwords, the first item in the
-	// slice is the first middleware applied, and the last item in the slice is the last middleware
-	// applied. Each response handler is called in the reverse order of the middleware. Meaning,
-	// the last middleware called will be the first to have its response handler called, and
-	// likewise, the first middleware called will be the last to have its handler called.
-	Middleware []ClientMiddleware
-}
 
 // RetryRoundTripper wraps a roundtripper with retry logic
 type RetryRoundTripper struct {
@@ -56,15 +37,17 @@ type RetryRoundTripper struct {
 	MaxRetries           uint8
 }
 
-// NewDefaultClient constructs the default HTTP Client with middleware. Providing an HTTP
-// RoundTripper is optional. If `nil` is received, the DefaultClient will be used.
+// NewDefaultClient constructs the default HTTP Client with a series of HTTP RoundTrippers that
+// provide additional features, such as exponential backoff, metrics, tracing, authentication
+// passthrough, and logging. Providing the base HTTP RoundTripper is optional.
+// If `nil` is received, the net/http DefaultClient will be used.
 //
 // By default, the client is provides exponential backoff on 500-504 errors. The default
 // configuration for exponential backoff is to start with an interval of 100 milliseconds, a
 // multiplier of two, a randomization factor of 0.5 (for jitter), a max interval of 10 seconds,
 // and finally, the retry will attempt 5 times before failing if the error is retriable.
 //
-// In addition, the middleware included will instrument HTTP calls with prometheus metrics,
+// In addition, the RoundTripper chain will instrument HTTP calls with prometheus metrics,
 // jaeger tracing, auth header passthrough, and zap logging.
 func NewDefaultClient(metrics Metrics, roundTripper http.RoundTripper) http.Client {
 	if roundTripper == nil {
@@ -84,55 +67,11 @@ func NewDefaultClient(metrics Metrics, roundTripper http.RoundTripper) http.Clie
 		RandomizationFactor: 0.5,
 		MaxRetries:          5,
 	}
-	return http.Client{
-		Transport: MiddlewareRoundTripper{
-			Middleware: []ClientMiddleware{
-				//tracing.HTTPClientMiddleware,
-				//log.HTTPClientMiddleware,
-				//metrics.ClientMiddleware,
-				//jose.HTTPClientMiddleware,
-			},
-			RoundTripper: retryRoundTripper,
-		},
-	}
-}
-
-// RoundTrip completes the http request round trip and is responsible for invoking HTTP Client
-// Middleware
-func (mrt MiddlewareRoundTripper) RoundTrip(req *http.Request) (*http.Response, error) {
-	// Ensure the RoundTripper was set on the MiddlewareRoundTripper
-	if mrt.RoundTripper == nil {
-		panic("no roundtripper provided to middleware round tripper")
-	}
-
-	// Call all middleware
-	numMiddleware := len(mrt.Middleware)
-	responseHandlers := make([]func(*http.Response) error, numMiddleware)
-	for idx, middleware := range mrt.Middleware {
-		updatedReq, callback, err := middleware(req)
-		if err != nil {
-			return nil, fmt.Errorf("error invoking http client middleware: %w", err)
-		}
-		// Append handlers in reverse order so that nesting is reverse on response handling.
-		// Always call the last middleware's response handler first, the second to last
-		// middleware's response handler second, and so on.
-		responseHandlers[numMiddleware-idx-1] = callback
-		req = updatedReq
-	}
-
-	// Make the request
-	resp, err := mrt.RoundTripper.RoundTrip(req)
-	if err != nil {
-		return nil, fmt.Errorf("http client request failed: %w", err)
-	}
-
-	// Call response handler callbacks with the response
-	for _, callback := range responseHandlers {
-		if err := callback(resp); err != nil {
-			return nil, fmt.Errorf("error invoking http client response handler middleware: %w", err)
-		}
-	}
-	return resp, err
+	tracingRoundTripper := tracing.RoundTripper{RoundTripper: retryRoundTripper}
+	loggingRoundTripper := log.RoundTripper{RoundTripper: tracingRoundTripper}
+	metricsRoundTripper := MetricsRoundTripper{RoundTripper: loggingRoundTripper}
+	joseRoundTripper := jose.RoundTripper{RoundTripper: metricsRoundTripper}
+	return http.Client{Transport: joseRoundTripper}
 }
 
 // RoundTrip completes the http request round trip but attempts retries for configured error codes
