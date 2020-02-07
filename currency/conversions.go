d65c3feaff06bd11dd91ccb7608ad7767aa2ce28
Benjamin Goldberg <ben@spothero.com>
2020-02-07 09:03:23 -0600

    PenniesInDollar should be exported

--- currency/conversions.go
+++ currency/conversions.go
@@ -2,17 +2,17 @@ package currency
 
 import "math"
 
-// penniesInDollar represents that there are 100 pennies in a dollar
-const penniesInDollar float64 = 100
+// PenniesInDollar represents that there are 100 pennies in a dollar
+const PenniesInDollar float64 = 100
 
 // DollarsToPennies converts a floating point value in dollars to pennies. If `roundUp` is true any
 // fractional value that is less than 1 penny will be rounded up and added as an additional penny.
 // Otherwise, that value will be discarded.
 func DollarsToPennies(dollars float64, roundUp bool) uint {
-	// Multiply the dollars amount by penniesInDollar to convert to pennies.
+	// Multiply the dollars amount by PenniesInDollar to convert to pennies.
 	// Split the floating point penny number around the decimal point. The left portion is the
 	// integer and the right part is the remaining fraction.
-	integerComponent, fractionComponent := math.Modf(dollars * penniesInDollar)
+	integerComponent, fractionComponent := math.Modf(dollars * PenniesInDollar)
 	// Convert the integerComponent (which is a float64) to a uint to get the base dollar amount
 	pennies := uint(integerComponent)
 	// If there is any additional remaining fractional amount, always round up to the nearest
