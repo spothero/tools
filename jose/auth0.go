b84699fa2f35d047e0908d8146db207cc22361a0
Michael Wilson <wilsoniya@gmail.com>
2020-03-06 16:32:18 -0600

    [PMP-480] adjustments to Auth0 claim functionality

--- jose/auth0.go
+++ jose/auth0.go
@@ -16,6 +16,7 @@ package jose
 
 import (
 	"context"
+	"fmt"
 )
 
 // Auth0CtxKey is the type used to uniquely place the cognito claim in the context
@@ -32,9 +33,9 @@ type Auth0Generator struct{}
 // JWTs, ClientID will be populated. For normal User authentication UserID will be populated. Both
 // UserID and ClientID will not be populated together.
 type Auth0Claim struct {
-	UserID   string `json:"sub"`
-	ClientID string `json:"aud"`
-	Email    string `json:"email"`
+	ID        string `json:"sub"`
+	Email     string `json:"email"`
+	GrantType string `json:"gty"`
 }
 
 // New satisfies the ClaimGenerator interface, returning an empty claim for use with JOSE parsing
@@ -46,4 +47,33 @@ func (cg Auth0Generator) New() Claim {
 // NewContext registers a claim to a given context and returns that new context
 func (cc Auth0Claim) NewContext(ctx context.Context) context.Context {
 	return context.WithValue(ctx, Auth0ClaimKey, &cc)
+}
+
+// GetClientID returns the ClientID field of the claim if it is present,
+// otherwise the empty string
+func (cc Auth0Claim) GetClientID() string {
+	if cc.GrantType == "client-credentials" {
+		return cc.ID
+	}
+
+	return ""
+}
+
+// GetUserID returns the UserID field of the claim if it is present, otherwise
+// the empty string
+func (cc Auth0Claim) GetUserID() string {
+	if cc.GrantType == "password" {
+		return cc.ID
+	}
+
+	return ""
+}
+
+// FromContext extracts an Auth0 claim from a context
+func FromContext(ctx context.Context) (*Auth0Claim, error) {
+	if claim, ok := ctx.Value(Auth0ClaimKey).(*Auth0Claim); ok {
+		return claim, nil
+	}
+
+	return nil, fmt.Errorf("unable to extract claim from given context")
 }
--- jose/auth0_test.go
+++ jose/auth0_test.go
@@ -28,11 +28,102 @@ func TestAuth0GeneratorNew(t *testing.T) {
 func TestAuth0ClaimNewContext(t *testing.T) {
 	ctx := context.Background()
 	cc := Auth0Claim{
-		UserID:   "abc123",
-		ClientID: "abc123",
-		Email:    "email",
+		ID:        "abc123",
+		Email:     "email",
+		GrantType: "client-credentials",
 	}
 
 	expected := context.WithValue(context.Background(), Auth0ClaimKey, &cc)
 	assert.Equal(t, expected, cc.NewContext(ctx))
+}
+
+func TestFromContext(t *testing.T) {
+	// GIVEN an auth0 claim is present in a context
+	ctx := context.Background()
+	cc := Auth0Claim{
+		ID:        "abc123",
+		Email:     "email",
+		GrantType: "client-credentials",
+	}
+	ctx = cc.NewContext(ctx)
+
+	// WHEN we attempt to extract an auth0 claim
+	maybeClaim, err := FromContext(ctx)
+
+	// THEN an auth0 claim can be extracted from that context
+	assert.NoError(t, err)
+	assert.NotNil(t, maybeClaim)
+	assert.Equal(t, cc, *maybeClaim)
+
+	// GIVEN an empty context
+	ctx2 := context.Background()
+
+	// WHEN we attempt to extract an auth0 claim
+	maybeClaim, err = FromContext(ctx2)
+
+	// THEN an error occurs
+	assert.Error(t, err)
+	assert.Nil(t, maybeClaim)
+}
+
+func TestGetClientID(t *testing.T) {
+	tests := []struct {
+		name     string
+		input    Auth0Claim
+		expected string
+	}{
+		{
+			name:     "client id present",
+			input:    Auth0Claim{"id", "email", "client-credentials"},
+			expected: "id",
+		},
+		{
+			name:     "user id present",
+			input:    Auth0Claim{"id", "email", "password"},
+			expected: "",
+		},
+		{
+			name:     "unknown grant",
+			input:    Auth0Claim{"id", "email", "BoGuS"},
+			expected: "",
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			actual := test.input.GetClientID()
+			assert.Equal(t, test.expected, actual)
+		})
+	}
+}
+
+func TestGetUserID(t *testing.T) {
+	tests := []struct {
+		name     string
+		input    Auth0Claim
+		expected string
+	}{
+		{
+			name:     "client id present",
+			input:    Auth0Claim{"client-id", "email", "client-credentials"},
+			expected: "",
+		},
+		{
+			name:     "user id present",
+			input:    Auth0Claim{"user-id", "email", "password"},
+			expected: "user-id",
+		},
+		{
+			name:     "unknown grant",
+			input:    Auth0Claim{"id", "email", "BoGuS"},
+			expected: "",
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			actual := test.input.GetUserID()
+			assert.Equal(t, test.expected, actual)
+		})
+	}
 }
