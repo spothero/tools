103d6e9891577aae3d1f1212e04614e5f801ff76
Jim Werwath <jim.werwath@spothero.com>
2020-07-12 07:46:11 -0500

    Better test

--- log/example_test.go
+++ log/example_test.go
@@ -5,11 +5,12 @@ import (
     "context"
 )
 
+// This example demonstrates how to initialize and create a logger.
 func ExampleCreateLogger() {
   c := Config{UseDevelopmentLogger: true}
   c.InitializeLogger()
-  logger := Get(context.Background())
-  fmt.Printf("%v", logger != nil)
-  // Output: true
-}
 
+  logger := Get(context.Background())
+  fmt.Printf("%T", logger)
+  // Output: *zap.Logger
+}
