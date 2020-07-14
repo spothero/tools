4c123e49af7b225889f3fee9c276b1561a5133b3
Jim Werwath <jim.werwath@spothero.com>
2020-07-14 07:54:01 -0500

    Add pagination package doc

---
+++ pagination/example_test.go
@@ -0,0 +1,26 @@
+package pagination
+
+import (
+    "fmt"
+)
+
+type Thing string
+
+func (o Thing) ExtractID() string {
+  return string(o)
+}
+
+func ExamplePagination() {
+  pages := []Pageable { 
+  Thing("foo"),
+  Thing("bar"),
+  Thing("baz"),
+  Thing("boo"),
+  Thing("bla"),
+  }
+
+  page := GetPageAfterID(pages, "bar", 2)
+
+  fmt.Printf("%v", page)
+  //Output: [baz, boo]
+}
