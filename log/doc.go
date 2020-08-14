c87fd06c6a8e0706dfc6366e4e807e4931891f9d
Jim Werwath <jim.werwath@spothero.com>
2020-08-14 10:41:23 -0500

    Use // comments in log pkg docs

--- log/doc.go
+++ log/doc.go
@@ -1,7 +1,4 @@
-/*
-Package log wraps the go.uber.org/zap log package.
-
-It provides methods that attach loggers to middleware, grpc servers, and http.RoundTrippers
-*/
+// Package log wraps the go.uber.org/zap log package.
+// It provides methods that attach loggers to middleware, grpc servers, and http.RoundTrippers
 
 package log
