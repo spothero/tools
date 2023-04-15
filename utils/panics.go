e45c791641c99e9b9c5bd09680804c27783a5cba
Nicholas Kincaid <nbkincaid@gmail.com>
2023-04-15 12:00:42 -0500

    SEAR-2604 add log panic

--- utils/panics.go
+++ utils/panics.go
@@ -7,9 +7,22 @@ import (
 	"go.uber.org/zap"
 )
 
-// CatchPanic prevents a panic from escaping and allows us to log an error for it
+// CatchPanic prevents a panic from escaping and allows us to log an error for it. This is helpful to keep track of
+// async goroutines that do not return anything to the main app thread. We want to catch the panic and log an error in
+// these cases and want to prevent them from crashing the app. Ideally this is used in a debugging process that
+// leads to the resolution of the panic case.
 func CatchPanic(ctx context.Context) {
 	if err := recover(); err != nil {
 		log.Get(ctx).Error("caught panic", zap.String("error", fmt.Sprint(err)))
+	}
+}
+
+// LogPanic produces an error log to record the issue and repanics to allow the app to crash. This is helpful to get a
+// definitive error log before the uncaught panic crashes the app. Ideally this is used in a debugging process that
+// leads to the resolution of the panic case.
+func LogPanic(ctx context.Context) {
+	if err := recover(); err != nil {
+		log.Get(ctx).Error("uncaught panic", zap.String("error", fmt.Sprint(err)))
+		panic(fmt.Sprint(err))
 	}
 }
--- utils/panics_test.go
+++ utils/panics_test.go
@@ -11,3 +11,15 @@ func TestCatchPanic(t *testing.T) {
 		panic("this gets caught")
 	})
 }
+
+func TestLogPanic(t *testing.T) {
+	t.Run("panic is not caught", func(t *testing.T) {
+		defer func() {
+			if r := recover(); r == nil {
+				t.Errorf("The code did not panic")
+			}
+		}()
+		defer LogPanic(context.Background())
+		panic("this passes through")
+	})
+}
